c2efc734025dab5f2327f16e8f63fecc41d90521
RenXusheng233 <782449909@qq.com>
2024-01-24 19:01:14 +0800

    chore(6.6-I/O): update

--- src/bin/echo-server-copy.rs
+++ src/bin/echo-server-copy.rs
@@ -1,28 +1,20 @@
 use tokio::{
-    io::{self, AsyncReadExt, AsyncWriteExt},
-    net::TcpStream,
+    io::{self},
+    net::TcpListener,
 };
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    let stream = TcpStream::connect("127.0.0.1:6142").await?;
-    let (mut rd, mut wt) = io::split(stream);
+    let listener = TcpListener::bind("127.0.0.1:6142").await?;
 
-    tokio::spawn(async move {
-        wt.write_all(b"hello\r\n").await?;
-        wt.write_all(b"world\r\n").await?;
-
-        Ok::<_, io::Error>(())
-    });
-
-    let mut buf = vec![0; 128];
     loop {
-        let n = rd.read(&mut buf[..]).await?;
-        if n == 0 {
-            break;
-        }
-        println!("Got {:?}", &buf[..n]);
-    }
+        let (mut socket, _) = listener.accept().await?;
+        tokio::spawn(async move {
+            let (mut rd, mut wt) = socket.split();
 
-    Ok(())
+            if io::copy(&mut rd, &mut wt).await.is_err() {
+                eprintln!("Failed to copy");
+            }
+        });
+    }
 }
